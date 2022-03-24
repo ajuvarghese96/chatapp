3a43b7fee1cb7218e22680877b8f9f9fda700d1a
Aju Varghese <ajuvarghese1996@gmail.com>
2022-03-24 11:15:01 +0530

    first commit

---
+++ chatapp/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    value = models.CharField(max_length=500)
+    date = models.DateTimeField(default=datetime.now,blank=True)
+    user = models.CharField(max_length=500)
+    room = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.user
